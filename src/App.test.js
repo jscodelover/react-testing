b1e583a9c59edce244db36f523c0836d5ce8c6eb
jscodelover <manisha.4918@gmail.com>
2020-05-19 16:06:54 +0530

    jest setup with initial test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	const wrapper = shallow(<App />);
+	expect(wrapper.find('a').text()).toEqual('React Testing');
+	console.log(wrapper.find('a').debug());
+});
+
+test('renders app logo', () => {
+	const wrapper = shallow(<App />);
+	expect(wrapper.find('.App-logo')).toHaveLength(1);
 });
